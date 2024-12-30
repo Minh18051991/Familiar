b20301f63349c0bf3cb63b3330548566a9d1dcaf
unknown <minhdhh91@gmail.com>
2024-12-30 17:04:32 +0700

    minh

--- Familiar/src/main/resources/familiar.sql
+++ Familiar/src/main/resources/familiar.sql
@@ -188,9 +188,10 @@ CREATE TABLE messages
 -- Message_Icons table (mới)
 CREATE TABLE message_icons
 (
+    message_icons_id INT AUTO_INCREMENT PRIMARY KEY,
     message_id INT NOT NULL,
     icon_id    INT NOT NULL,
-    PRIMARY KEY (message_id, icon_id),
+    UNIQUE KEY unique_message_icon (message_id, icon_id),
     FOREIGN KEY (message_id) REFERENCES messages (message_id),
     FOREIGN KEY (icon_id) REFERENCES icons (icon_id)
 );
