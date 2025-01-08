69e93c3fb30054a4cf8fc34e5a7cfaeec4bff70e
unknown <minhdhh91@gmail.com>
2025-01-08 16:26:03 +0700

    minh

--- Familiar/src/main/java/org/example/familiar/service/user/IUserService.java
+++ Familiar/src/main/java/org/example/familiar/service/user/IUserService.java
@@ -3,6 +3,8 @@ package org.example.familiar.service.user;
 import org.example.familiar.dto.userDTO.UserDTO;
 import org.example.familiar.dto.userDTO.UserDTOImpl;
 import org.example.familiar.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IUserService {
     UserDTO findById(Integer id);
@@ -10,4 +12,5 @@ public interface IUserService {
     User getUserById(int id);
     User getUserByEmail(String email);
     boolean checkEmailExistence(String email);
+    Page<UserDTO> searchUsers(String keyword, Pageable pageable);
 }
