cdc8d02fe2099646ab3d9dffe6d1842da32403d9
hungnguyen988 <hungnguuen988@gmail.com>
2025-01-22 15:09:32 +0700

    first

--- Familiar/src/main/java/org/example/familiar/controller/WebSocketMessageController.java
+++ Familiar/src/main/java/org/example/familiar/controller/WebSocketMessageController.java
@@ -9,8 +9,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin("*")
 public class WebSocketMessageController {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketMessageController.class);
